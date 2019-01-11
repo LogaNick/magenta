59777e295ab68a44b9f8d2ec8e26a5a6f7c6f83f
nick <nick-m@hotmail.ca>
2019-01-11 10:05:10 -0400

    Updated dataset creation scripts

---
+++ dataset_creation_scripts/tfrecord_to_examples.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]
+then
+  echo "Usage: $0 <in dir> <out dir> <config>"
+  exit 1
+fi
+
+NOTESEQUENCES_FILE=$1
+OUTPUT_DIRECTORY=$2
+CONFIG=$3
+
+cd ~/magenta
+source activate magenta
+
+nohup ./bazel-bin/magenta/models/performance_rnn/performance_rnn_create_dataset \
+--config=${CONFIG} \
+--input=$NOTESEQUENCES_FILE \
+--output_dir=$OUTPUT_DIRECTORY \
+--eval_ratio=0.10
